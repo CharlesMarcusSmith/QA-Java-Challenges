def91699b1977d6e7fdb53b490d8ef0d7090b6ae
llamaladejam <charliemarcussmith@googlemail.com>
2022-12-23 13:06:44 +0000

    CS: String representation completed for the array challenges.

--- QAJavaChallenges/src/com/qa/javachallenges/arrays/Runner.java
+++ QAJavaChallenges/src/com/qa/javachallenges/arrays/Runner.java
@@ -14,19 +14,24 @@ public class Runner {
 		Numbers num = new Numbers();
 //		Create a method that takes a number 10-99, and adds the two digits together for example 74 = 7 + 4 = 11.
 		num.numm1(63);
-		num.numm2assembly(num.numm2a(93));
-		num.numm2assembly(num.numm2a(1345));
-		num.numm2assembly(num.numm2a(6005));
-		num.numm2assembly(num.numm2a(7300));
-		num.numm2assembly(num.numm2a(6015));
-		num.numm2assembly(num.numm2a(5));
-		num.numm2assembly(num.numm2a(13));
-		num.numm2assembly(num.numm2a(1030));
+		
+//		TESTING:
+//		num.numm2assembly(num.numm2a(93));
+//		num.numm2assembly(num.numm2a(1345));
+//		num.numm2assembly(num.numm2a(6005));
+//		num.numm2assembly(num.numm2a(7300));
+//		num.numm2assembly(num.numm2a(6015));
+//		num.numm2assembly(num.numm2a(5));
+//		num.numm2assembly(num.numm2a(13));
+//		num.numm2assembly(num.numm2a(1030));
+		
 //		Create a second method that when given the number 1-99 returns a String representation of this number, for example 1 = one, 11 = eleven, 21 = twenty-one.
 //		Expand on the method you wrote in step 2 to allow the input 1-999.
 //		Expand the method you wrote in step 2 to allow the input 1-9999.
 //		Use a for()-loop to print the numbers 1-100 in words.
-		
+		for(int i = 1; i<=100; i++) {
+			num.numm2assembly(num.numm2a(i));
+		}
 //		For example; 1 = one, 100 = one hundred.
 	}
-}
+} 
